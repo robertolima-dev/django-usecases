51b59c4a201beeb9f29d53977ddda7d584dd497f
Roberto Lima <roberto.rigamonti.lima@gmail.com>
2025-04-10 11:33:26 -0300

    feat: Iniciando CI/CD

--- start_server.sh
+++ start_server.sh
@@ -6,14 +6,22 @@ python manage.py collectstatic --noinput
 echo "🛠️ Aplicando migrações..."
 python manage.py migrate
 
-echo "🚀 Iniciando Daphne (ASGI)..."
-daphne -b 0.0.0.0 -p 8000 api_core.asgi:application &
-
-echo "⚙️ Iniciando Celery Worker..."
-celery -A api_core worker --loglevel=info &
-
-echo "⏰ Iniciando Celery Beat..."
-celery -A api_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
-
-# Mantém o container vivo
-tail -f /dev/null
+# Executa baseado no modo
+case "$1" in
+  web)
+    echo "🚀 Iniciando Daphne (ASGI)..."
+    exec daphne -b 0.0.0.0 -p 8000 api_core.asgi:application
+    ;;
+  worker)
+    echo "⚙️ Iniciando Celery Worker..."
+    exec celery -A api_core worker --loglevel=info
+    ;;
+  beat)
+    echo "⏰ Iniciando Celery Beat..."
+    exec celery -A api_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler
+    ;;
+  *)
+    echo "❌ Modo inválido. Use: web, worker ou beat"
+    exit 1
+    ;;
+esac
